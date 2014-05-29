fcbf168eb9ff4fdffe80c24c6ebb1755e70e073f
Hellthrash <hellthrash@gmail.com>
2014-05-29 11:27:47 -0400

    Elimine archivos de prueba

--- INFO175/Ejercicios/Ejercicioenclase1.py
+++
@@ -1,5 +0,0 @@
-x = input("ingrese el largo de su primera lista: ")
-a = []
-for i in range(x):
-    a.append(raw_input("Ingresa una palabra \n"))
-print a
--- INFO175/Ejercicios/Problema2.py
+++
@@ -1,8 +0,0 @@
-x = input("ingrese primer numero: ")
-y = input("Ingrese el segundo numero: ")
-if(x == y):
-    print "Los numeros son iguales"
-elif(x > y):
-    print x, "es mayor que ", y
-else:
-    print y, "es mayor que ", x
