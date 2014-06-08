1259f559a07b75d21060a9e2ab5c43cdafa2d28f
Hellthrash <hellthrash@gmail.com>
2014-06-08 18:09:50 -0400

    Actualizacion funcional

--- QT/bd.py
+++ QT/bd.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import sqlite3
@@ -7,11 +8,20 @@ def conectar():
     con.row_factory = sqlite3.Row
     return con
 
-def obtener():
-    con = conectar()
-    c = con.cursor()
-    query = "SELECT * FROM  producto"
-    resultado = c.execute(query)
-    producto = resultado.fetchall()
-    con.close()
-    return producto
+def obtener(filtro):
+    if filtro == "all":
+        con = conectar()
+        c = con.cursor()
+        query = "SELECT * FROM  producto"
+        resultado = c.execute(query)
+        producto = resultado.fetchall()
+        con.close()
+        return producto
+    else:
+        con = conectar()
+        c = con.cursor()
+        query = "SELECT * FROM producto WHERE fk_id_marca=" + filtro
+        resultado = c.execute(query)
+        producto = resultado.fetchall()
+        con.close()
+        return producto
