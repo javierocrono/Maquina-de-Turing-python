6811acf4749eb9bd6aac37d58a91095144f71511
Hellthrash <hellthrash@gmail.com>
2014-06-08 18:27:10 -0400

    respaldando >_>

---
+++ QTRespaldo/bd.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+def conectar():
+    con = sqlite3.connect('producto.db')
+    con.row_factory = sqlite3.Row
+    return con
+
+def obtener(filtro):
+    if filtro == "all":
+        con = conectar()
+        c = con.cursor()
+        query = "SELECT * FROM  producto"
+        resultado = c.execute(query)
+        producto = resultado.fetchall()
+        con.close()
+        return producto
+    else:
+        con = conectar()
+        c = con.cursor()
+        query = "SELECT * FROM producto WHERE fk_id_marca=" + filtro
+        resultado = c.execute(query)
+        producto = resultado.fetchall()
+        con.close()
+        return producto
+
+#ASD
