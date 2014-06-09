ea9a0e04862fb4be7cbc83bac0f80be5b933a858
Javiero <juglerhak@hotmail.com>
2014-06-08 23:50:15 -0400

    lista interfaz de inicio

--- QT/inicio.py
+++ QT/inicio.py
@@ -12,12 +12,15 @@ import visual
 class Principal(QtGui.QWidget, ventanaCredenciales.Ui_ventanaCredenciales):
 
     def __init__(self, parent=None):
+
         super(Principal, self).__init__(parent)
         self.setupUi(self)
         self.move(450, 200)
         self.buttonAceptar.clicked.connect(self.sesion)
         self.buttonRegistro.clicked.connect(self.registro)
         self.main_layout = QtGui.QVBoxLayout(self)
+
+        self.enRegistro = False
         self.show()
 
     def sesion(self):
@@ -31,7 +34,7 @@ class Principal(QtGui.QWidget, ventanaCredenciales.Ui_ventanaCredenciales):
         else:
             if (usuarios[0][2] == contrasenia):
                 msgbox = QMessageBox()
-                msgbox.setText("Bienvenido" + nombre)
+                msgbox.setText("Bienvenido " + nombre)
                 msgbox.exec_()
                 self.close()
             else:
@@ -40,8 +43,36 @@ class Principal(QtGui.QWidget, ventanaCredenciales.Ui_ventanaCredenciales):
                 msgbox.exec_()
 
     def registro(self):
-        self.buttonRegistro.hide()
-        self.setWindowTitle("Registro")
+        if (self.enRegistro is False):
+            self.buttonAceptar.hide()
+            self.buttonRegistro.move(170,230)
+            self.setWindowTitle("Registro")
+            self.enRegistro =True
+        else:
+            nombre = self.lineNombre.text()
+            if (nombre != ""):
+                usuarios = manejoUsuarios.existe(nombre)
+                contrasenia = self.lineContrasenia.text()
+                if (usuarios == []):
+                    msgbox = QMessageBox()
+                    msgbox.setText("Usuario Valido")
+                    msgbox.exec_()
+                    if (contrasenia != ""):
+                        manejoUsuarios.crear(nombre, contrasenia)
+                        msgbox.setText("Usuario exitosamente Registrado")
+                        msgbox.exec_()
+                        self.buttonRegistro.move(30,230)
+                        self.buttonAceptar.show()
+                        self.enRegistro = False
+                        self.setWindowTitle("Inicio de Sesion")
+                else:
+                    msgbox = QMessageBox()
+                    msgbox.setText("Usuario Invalido")
+                    msgbox.exec_()
+            else:
+                msgbox = QMessageBox()
+                msgbox.setText("Usuario Invalido")
+                msgbox.exec_()
 
 
 def run():
